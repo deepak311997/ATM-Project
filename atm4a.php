efcd3e3e3d14ceae57a780c49926c7142992a615
gauthamn46 <32977577+gauthamn46@users.noreply.github.com>
2017-11-09 15:52:33 +0530

    no idea wat
    4pm

--- atm4a.php
+++ atm4a.php
@@ -1,5 +1,4 @@
 <!--withdraw cash-->
-<!DOCTYPE html>
 <html>
 <head>
 	<title>WITHDRAW MONEY</title>
@@ -9,12 +8,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" href="icon.png" type="image/x-icon"/>
 <style>
-input[type=submit]
-{
-	background-color:gray;
-	width: 15%;
-	color: white
-}
 button {
   color: #000000;
   font-weight: bold;
@@ -28,12 +21,12 @@ button {
 <img src="mylogo.png" alt="State Bank of India">
 <br>
 <br>
-<form action="index.php">
+<form action="withdrawal_check.php" method="POST">
 <label>Withdrawal Amount :</label>
-  <input id="amount" type="number" placeholder="Amount" >
+  <input name="amount" type="number" placeholder="Amount" >
   <br>
   <br>
-  <button type="submit" onclick="IDGeneratorwith(10000000,99999999)">Withdraw</button>
+  <button type="submit">Withdraw</button>
 </body>
 <footer><b>Copyrights &copy; Students Bank Public Limited 2018</b></footer>
 </html>
---
+++ withdrawal_check.php
@@ -0,0 +1,37 @@
+<script>
+		function IDGeneratorwith(min,max)
+	{
+		var refid=Math.floor(Math.random() * (max - min + 1) + min);
+		alert("Withdrawal Successfull your Transaction id is "+refid+"\nThank You for banking with us");
+
+	}
+</script>
+
+
+
+
+<?php 
+	session_start();
+	$conn=mysqli_connect('localhost','root','','atm');
+	$amt=$_POST['amount'];
+	$sql="select saving_balance from saving_account where c_id=select c_id from account_details where acc_no='$_SESSION[db_usr]'";
+	$result=mysqli_query($conn,$sql);
+	if (mysqli_num_rows('$result') > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) 
+    {
+		 		if ($row["saving_balance"]>=$amt) 
+		 		{
+		 			$actamt=$row["saving_balance"]-$amt;
+		 			$sql="update saving_account set saving_balance =$actamt WHERE c_id=select c_id from account_details where acc_no='$_SESSION[db_usr]'";
+		 			echo "HIII";
+		 		}
+				/*else
+				{
+					echo '<script language="javascript">';
+					echo 'alert(" You have Insufficient funds in your account!!");window.location="index.php"';
+					echo '</script>';
+				}*/
+	}
+}
+ ?>
