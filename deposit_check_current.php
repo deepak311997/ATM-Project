22720609f2d16ef2fd2ff421f80d15d0e17cb881
gauthamn46 <32977577+gauthamn46@users.noreply.github.com>
2017-11-10 11:29:51 +0530

    1130am

--- deposit_check_current.php
+++ deposit_check_current.php
@@ -10,18 +10,27 @@
  	session_start();
 	$conn=mysqli_connect('localhost','root','','atm');
 	$amt=$_POST['amount'];
-	$sql="select current_balance from current_account where c_id=(select c_id from account_details where acc_no='$_SESSION[db_usr]');";
-	$result=mysqli_query($conn,$sql);
-	if (mysqli_num_rows($result)>0) 
+	if ($amt%100==0) 
 	{
-		while($row=mysqli_fetch_assoc($result)) 
+		$sql="select current_balance from current_account where c_id=(select c_id from account_details where acc_no='$_SESSION[db_usr]');";
+		$result=mysqli_query($conn,$sql);
+		if (mysqli_num_rows($result)>0) 
 		{
+			while($row=mysqli_fetch_assoc($result)) 
+			{
 		 		$actamt=$row["current_balance"]+$amt;
 		 		$sql1="update current_account set current_balance =$actamt WHERE c_id=(select c_id from account_details where acc_no='$_SESSION[db_usr]')";
 		 		mysqli_query($conn,$sql1);
 		 		echo '<script language="javascript">';
 				echo 'alert("Thank you for banking with us !!");window.location="index.php"';
 				echo '</script>';	
-    	}
+    		}
+		}
+	}
+	else 
+	{
+	 	echo '<script language="javascript">';
+		echo 'alert("Please enter amount in multiples of 100!");window.location="index.php"';
+		echo '</script>';
 	} 
  ?>
